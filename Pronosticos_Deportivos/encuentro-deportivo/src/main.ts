c51cba8bfa2feaa8a32a9a83d4bcb8c525585210
Nagely Chancay <e1350176721@live.uleam.edu.ec>
2022-11-05 17:52:30 -0500

    Aplicación de Swagger

--- Pronosticos_Deportivos/encuentro-deportivo/src/main.ts
+++ Pronosticos_Deportivos/encuentro-deportivo/src/main.ts
@@ -1,8 +1,23 @@
+//Importación de liberías
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+//Metodo 
 async function bootstrap() {
+  //Constantes
   const app = await NestFactory.create(AppModule);
+  //Constante y datos
+  const options = new DocumentBuilder()
+  .setTitle('Pronósticos Deportivos')
+  .setDescription('Este servicio está destinado al registro y almacenamiento de Pronósticos Deportivos')
+  .setVersion('1.0')
+  .addTag('Pronóstico')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  //Ruta para acceder a Swagger
+  SwaggerModule.setup('pronostico/docs', app, document);
+  //Puerto
   await app.listen(3000);
 }
 bootstrap();
