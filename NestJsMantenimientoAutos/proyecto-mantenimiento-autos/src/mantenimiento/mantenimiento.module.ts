f1d3e6e953daa8a42505800f27017991d7366dda
MrFlexEc <itsthebryanyt@gmail.com>
2022-11-07 02:21:07 -0500

    Correcion de error en el modulo Mantenimiento Module, hacia falta importar MongooseModule

--- NestJsMantenimientoAutos/proyecto-mantenimiento-autos/src/mantenimiento/mantenimiento.module.ts
+++ NestJsMantenimientoAutos/proyecto-mantenimiento-autos/src/mantenimiento/mantenimiento.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MantenimientoController } from './mantenimiento.controller';
 import { MantenimientoService } from './mantenimiento.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { MantenimientoSchema } from './schema/mantenimiento.schema';
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Mantenimiento', schema: MantenimientoSchema },
+    ]),
+  ],
   controllers: [MantenimientoController],
   providers: [MantenimientoService],
 })
